b6803f4c2c4ca3324bdd9048a4c8116fc6f0431d
Otto Werse <otto.werse@web.de>
2021-11-23 18:23:26 +0100

    opened a window.

--- gui/src/main.java
+++ gui/src/main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class main {
     public static void main(String[] args) {
-
+        JFrame f=new JFrame();
+        f.setSize(800,400);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
