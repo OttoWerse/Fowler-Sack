e07878df7e9290c018598df10edc4525ce8f7ca8
Otto Werse <otto.werse@web.de>
2021-11-23 19:01:41 +0100

    Labeled some stuff

--- gui/src/Window.java
+++ gui/src/Window.java
@@ -1,10 +1,37 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Window {
     public static void main(String[] args) {
         JFrame f=new JFrame();
+
         f.setSize(800,400);
-        f.setLayout(null);
+        f.setLayout(new GridLayout(1, 2));
+
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(5,2));
+        p.setVisible(true);
+
+        JScrollPane s = new JScrollPane();
+
+        p.add(new JLabel("Station ID"));
+        p.add(new JTextField());
+
+        p.add(new JLabel("Date"));
+        p.add(new JTextField());
+
+        p.add(new JLabel("Target"));
+        p.add(new JTextField());
+
+        p.add(new JLabel("Actual"));
+        p.add(new JTextField());
+
+        p.add(new JLabel("Variance"));
+        p.add(new JTextField());
+
+        f.add(s);
+        f.add(p);
+
         f.setVisible(true);
     }
 }
